4a2363ce07c9c59ae7acce75756c5716c9afca5f
wyllowyllo <minguan51910@gmail.com>
2022-05-19 09:36:46 +0900

    시점이동 translate 구현
    .

--- main.cpp
+++ main.cpp
@@ -1,72 +1,89 @@
-﻿#include<glut.h>
+﻿#ifdef _WIN32
+#include <windows.h>
+#include<glut.h>
 #include<glu.h>
 #include<gl.h>
-
-#include <iostream>
-#include <cstdlib>
-#include <time.h>
+#ifdef _WIN64
 #include <windows.h>
+#include<glut.h>
+#include<glu.h>
+#include<gl.h>
+#endif
+#elif __APPLE__
+#include "TargetConditionals.h"
+#if TARGET_IPHONE_SIMULATOR
+#include<GLUT/glut.h>
+#elif TARGET_OS_IPHONE
+#include<GLUT/glut.h>
+#elif TARGET_OS_MAC
+#include<GLUT/glut.h>
+#else
+#   error "Unknown Apple platform"
+#endif
+#elif __linux__
+// linux
+#elif __unix__ // all unices not caught above
+// Unix`
+#elif defined(_POSIX_VERSION)
+// POSIX
+#else
+#   error "Unknown compiler"
+#endif
+
+#include "map.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #pragma comment (lib, "winmm.lib")
 #include <mmsystem.h>;
 
-#include "map.h"
-#include "location.h"
-
-using namespace std;
 
 
 #define SOUND_FILE_GUN_FIRE "sounds/Gun_Fire.wav"
 #define SOUND_FILE_GUN_RELOAD "sounds/Gun_reload.wav"
 
 
-//Lookat 변수
-double Camera_center[3] = { 0,0,-1 }, Camera_up[3] = { 0,1,0 };
-//마우스 시점 이동 변수
-GLint Camera_mouse[2] = { 0,0 };
 
-GLint FullwindowX = 1600, FullwindowY = 900;
-GLfloat screen_Sensitivity_X = 10000, screen_Sensitivity_Y = 0.00000005;
-
-Location player(0,4,0);
+GLfloat pitchX, yawY; //카메라 x축, y축 회전각
+int FirstMouseMove = 1;
+GLfloat CurrentX, CurrentY; //현재 마우스 좌표
 
 
+void FpsView(GLfloat pitch, GLfloat yaw) {
+
+	glRotatef(pitch, 0.0, 1.0, 0.0);
+	glRotatef(yaw, 1.0, 0.0, 0.0);
+
+}
 void MyDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	Doom_map();
-	gluLookAt( // x,y,z
-		player.x, player.y, player.z, //eye
-		Camera_center[0], Camera_center[1], Camera_center[2], //center
-		Camera_up[0], Camera_up[1], Camera_up[2] //up
-	);
-
+	FpsView(pitchX, yawY);
 	glutSwapBuffers();
 }
 
 void MyReshape(int NW, int NH) {
 	GLfloat nRange = 3.0f;
-	FullwindowX = NW;
-	FullwindowY = NH;
-
 
 	if (NH == 0) {
 		NH = 1;
 	}
-	glViewport(0, 0, NW, NH);
 
+	glViewport(0, 0, NW, NH);
+	gluLookAt(0, 0, 21, 0, 0, -10, 0, 1, 0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
+
 	if (NW <= NH) {
 		glOrtho(-nRange, nRange, -nRange * NH / NW, nRange * NH / NW, -nRange, nRange);
 	}
 	else {
 		glOrtho(-nRange * NW / NH, nRange * NW / NH, -nRange, nRange, -nRange, nRange);
 	}
-	gluPerspective(50.0f, (GLfloat)NW / (GLfloat)NH, 1.0, 40.0);
+	gluPerspective(60, (NH / NW) / 2, -25, 21);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	
-	glutPostRedisplay();
-	
+
 }
 
 void MyKeyBoard(unsigned char KeyPressed, int X, int Y) {
@@ -74,25 +91,27 @@ void MyKeyBoard(unsigned char KeyPressed, int X, int Y) {
 	switch (KeyPressed)
 	{
 	case 'w':
-		player.z -= (double)0.05;
+		glTranslated(0.0, 0.0, 0.1);
 		break;
 	case 'a':
-		player.x -= (double)0.05;
+		glTranslated(0.1, 0.0, 0.0);
 		break;
 	case 's':
-		player.z += (double)0.05;
+		glTranslated(0.0, 0.0, -0.1);
 		break;
 	case 'd':
-		player.x += (double)0.05;
+		glTranslated(-0.1, 0.0, 0.0);
 		break;
+
 	case 'r':
 		printf("GUN_RELOAD\n");
-		//PlaySound(TEXT(SOUND_FILE_GUN_RELOAD), NULL, SND_ASYNC);
+		PlaySound(TEXT(SOUND_FILE_GUN_RELOAD), NULL, SND_ASYNC);
 		break;
+
+
 	default:
 		break;
 	}
-	cout << "x:" << player.x << ",y:" << player.y << ",z:" << player.z << endl;
 	glutPostRedisplay();
 }
 
@@ -103,7 +122,7 @@ void MySpecial(int key, int X, int Y) {
 void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y) {
 	if (Button == GLUT_LEFT_BUTTON && State == GLUT_DOWN) {
 		printf("GUN_FIRE\n");
-		//PlaySound(TEXT(SOUND_FILE_GUN_FIRE), NULL, SND_ASYNC);
+		PlaySound(TEXT(SOUND_FILE_GUN_FIRE), NULL, SND_ASYNC);
 	}
 }
 
@@ -113,35 +132,25 @@ void MyMouseMove(GLint X, GLint Y) {
 
 //마우스로 시점 변환시 사용
 void MyMousePassiveMove(GLint X, GLint Y) {
-	//printf("%i, %i\n", X, Y);
-
-	//if (X > Camera_mouse[0]) {
-	//	//오른쪽으로 시점이동
-	//	printf("오른쪽으로 시점이동\n");
-	//	Camera_center[0] += 0.00005;
-	//}
-	//else {
-	//	printf("왼쪽으로 시점이동\n");
-	//	Camera_center[0] -= 0.00005;
-	//}
-	//if (Y < Camera_mouse[1]) {
-	//	printf("위로 시점이동\n");
-	//	Camera_center[1] += 0.00005;
-	//}
-	//else {
-	//	printf("아래로 시점이동\n");
-	//	Camera_center[1] -= 0.00005;
-	//}
+	printf("%i, %i\n", X, Y);
 
 
-	//Camera_mouse[0] = X;
-	//Camera_mouse[1] = Y;
 
+	if (FirstMouseMove) {
+		CurrentX = X;
+		CurrentY = Y;
+		FirstMouseMove = 0;
+	}
+	pitchX = (X - CurrentX) * 0.3;
 
-	printf("%lf\n", (double)X / screen_Sensitivity_X - (double)FullwindowX / screen_Sensitivity_X / 2);
+	yawY = (Y - CurrentY) * 0.3;
+	if (yawY >= 60)
+		yawY = 60;
+	else if (yawY <= -60)
+		yawY = -60;
 
-	Camera_center[0] = (double)X / screen_Sensitivity_X - (double)FullwindowX / screen_Sensitivity_X / 2;
-
+	CurrentX = X;
+	CurrentY = Y;
 
 	glutPostRedisplay();
 
@@ -163,31 +172,27 @@ void MenuFunc() {
 
 }
 
-void userinit() {
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(1600, 900);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Doom");
 	glClearColor(0.0, 0.0, 0.0, 1.0);
-}
 
 
-int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	userinit();
-
-	//콜백 함수 등록
 	glutDisplayFunc(MyDisplay);
 	glutReshapeFunc(MyReshape);
 	glutKeyboardFunc(MyKeyBoard);
-	glutMouseFunc(MyMouseClick);
-	//glutPassiveMotionFunc(MyMousePassiveMove);
 	//glutSpecialFunc(MySpecial);
+	glutMouseFunc(MyMouseClick);
 	//glutMotionFunc(MyMouseMove);
+	glutPassiveMotionFunc(MyMousePassiveMove);
 	//glutIdleFunc(MyIdle);
 	//glutTimerFunc(40, MyTimer, 1);
 	//MenuFunc();
-	glutMainLoop();
 
+	glutMainLoop();
 	return 0;
 }
