77d489bd0e43472fefe7a8c98e871a65dd09117e
wyllowyllo <minguan51910@gmail.com>
2022-05-23 23:10:09 +0900

    좌우 시점이동, wasd이동 구현
    glortho->glperpective로 변경(원근감)
    카메라 위치변경->gluLookat
    마우스 이동시 y축 기준 회전
    gltranslate로 wasd이동 구현

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #pragma comment (lib, "winmm.lib")
 #include <mmsystem.h>;
 
@@ -13,49 +14,41 @@
 
 #define SOUND_FILE_GUN_FIRE "sounds/Gun_Fire.wav"
 #define SOUND_FILE_GUN_RELOAD "sounds/Gun_reload.wav"
+#define PI 3.1415
 
 
+GLfloat yawX, pitchY; //카메라 y축, x축 기준  회전각 변화량
+GLfloat CurrentX = 0.0f, CurrentY = 0.0f; //현재 마우스 좌표
+GLfloat moveX = 0.0f, moveZ = 0.0f; // X,Z축 시점 이동변화량
+GLfloat mX = 0.0f, mZ = 0.0f; // X,Z축 총 이동량
+GLfloat rotX = 0.0f, rotY = 0.0f; //FpsView func 전달인자, 총 회전각
 
-GLfloat pitchX, yawY; //카메라 x축, y축 회전각
-int FirstMouseMove = 1;
-GLfloat CurrentX, CurrentY; //현재 마우스 좌표
 
-
-void FpsView(GLfloat pitch, GLfloat yaw) {
-
-	glRotatef(pitch, 0.0, 1.0, 0.0);
-	glRotatef(yaw, 1.0, 0.0, 0.0);
-
+void FpsView(GLfloat yaw, GLfloat pitch) {
+	gluLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+	glRotatef(yaw, 0.0, 1.0, 0.0);
+	glRotatef(pitch, 1.0, 0.0, 0.0);
 }
+
 void MyDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushMatrix();
+	FpsView(rotX, rotY);
+	glTranslated(mX, 0, mZ);
 	Doom_map();
-	FpsView(pitchX, yawY);
+	glPopMatrix();
+
 	glutSwapBuffers();
 }
 
 void MyReshape(int NW, int NH) {
-	GLfloat nRange = 3.0f;
-
-	if (NH == 0) {
-		NH = 1;
-	}
-
 	glViewport(0, 0, NW, NH);
-	gluLookAt(0, 0, 21, 0, 0, -10, 0, 1, 0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
-	if (NW <= NH) {
-		glOrtho(-nRange, nRange, -nRange * NH / NW, nRange * NH / NW, -nRange, nRange);
-	}
-	else {
-		glOrtho(-nRange * NW / NH, nRange * NW / NH, -nRange, nRange, -nRange, nRange);
-	}
-	gluPerspective(60, (NH / NW) / 2, -25, 21);
+	gluPerspective(60.0f, NW / NH, 1, 22);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
 }
 
 void MyKeyBoard(unsigned char KeyPressed, int X, int Y) {
@@ -63,16 +56,32 @@ void MyKeyBoard(unsigned char KeyPressed, int X, int Y) {
 	switch (KeyPressed)
 	{
 	case 'w':
-		glTranslated(0.0, 0.0, 0.1);
+		moveX = sin((rotX) * (PI / 180));
+		moveZ = cos((rotX) * (PI / 180));
+
+		mX += -moveX;
+		mZ += moveZ;
 		break;
 	case 'a':
-		glTranslated(0.1, 0.0, 0.0);
+		moveX = sin((rotX) * (PI / 180) + (PI / 2));
+		moveZ = cos((rotX) * (PI / 180) + (PI / 2));
+
+		mX += moveX;
+		mZ += -moveZ;
 		break;
 	case 's':
-		glTranslated(0.0, 0.0, -0.1);
+		moveX = sin((rotX) * (PI / 180));
+		moveZ = cos((rotX) * (PI / 180));
+
+		mX += moveX;
+		mZ += -moveZ;
 		break;
 	case 'd':
-		glTranslated(-0.1, 0.0, 0.0);
+		moveX = sin((rotX) * (PI / 180) + (PI / 2));
+		moveZ = cos((rotX) * (PI / 180) + (PI / 2));
+
+		mX += -moveX;
+		mZ += moveZ;
 		break;
 
 	case 'r':
@@ -106,26 +115,13 @@ void MyMouseMove(GLint X, GLint Y) {
 void MyMousePassiveMove(GLint X, GLint Y) {
 	printf("%i, %i\n", X, Y);
 
-
-
-	if (FirstMouseMove) {
-		CurrentX = X;
-		CurrentY = Y;
-		FirstMouseMove = 0;
-	}
-	pitchX = (X - CurrentX) * 0.3;
-
-	yawY = (Y - CurrentY) * 0.3;
-	if (yawY >= 60)
-		yawY = 60;
-	else if (yawY <= -60)
-		yawY = -60;
-
+	yawX = X - CurrentX;
+	//pitchY = Y-CurrentY;
+	rotX += yawX * 0.5;
+	//rotY += pitchY * 0.5;
 	CurrentX = X;
-	CurrentY = Y;
-
+	//CurrentY = Y;
 	glutPostRedisplay();
-
 }
 
 void MyIdle() {
